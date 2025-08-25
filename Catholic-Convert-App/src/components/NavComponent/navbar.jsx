bcbd1bbf810bc68e422c2fac5b8587ee851c23bf
Phobic1 <padenw28@gmail.com>
2025-08-24 21:25:42 -0400

    Added Navbar

---
+++ Catholic-Convert-App/src/components/NavComponent/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      <nav class = "navbar">
+        <div class = "navbar-item">
+          Home
+        </div>
+        <div class = "navbar-item">
+          Find a Church
+        </div>
+        <div class = "navbar-item">
+          About
+        </div>
+        <div class = "navbar-item">
+          Contact Us
+        </div>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
